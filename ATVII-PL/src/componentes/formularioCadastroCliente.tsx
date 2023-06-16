9f0191adc7c5a9693e910e2efe90e43d267abc25
Igor Pereira <igorpereir28@hotmail.com>
2023-06-16 13:57:23 -0300

    Atividade 2 alterando e adicionando array para cadastrar mais de 1 fonte

--- ATVII-PL/src/componentes/formularioCadastroCliente.tsx
+++ ATVII-PL/src/componentes/formularioCadastroCliente.tsx
@@ -1,40 +1,160 @@
-import { Component } from "react";
+import React, { Component, ChangeEvent } from "react";
 
-type props = {
-    tema: string
-}
+type Props = {
+  tema: string;
+};
 
-export default class FormularioCadastroCliente extends Component<props> {
+type State = {
+  telefones: string[];
+  rg: string[];
+};
 
-    render() {
-        let tema = this.props.tema
-        return (
-            <div className="container-fluid">
-                <form>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Nome" aria-label="Nome" aria-describedby="basic-addon1" />
-                    </div>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Nome social" aria-label="Nome social" aria-describedby="basic-addon1" />
-                    </div>
-                    <div className="input-group mb-3">
-                        <span className="input-group-text" id="basic-addon1" style={{ background: tema }}>@</span>
-                        <input type="text" className="form-control" placeholder="E-mail" aria-label="E-mail" aria-describedby="basic-addon1" />
-                    </div>
-                    <div className="input-group mb-3">
-                        <input type="number" className="form-control" placeholder="CPF" aria-label="CPF" aria-describedby="basic-addon1" />
-                    </div>
-                    <div className="input-group mb-3">
-                        <input type="number" className="form-control" placeholder="RG" aria-label="RG" aria-describedby="basic-addon1" />
-                    </div>
-                    <div className="input-group mb-3">
-                        <input type="number" className="form-control" placeholder="Telefone" aria-label="Telefone" aria-describedby="basic-addon1" />
-                    </div>
-                    <div className="input-group mb-3">
-                        <button className="btn btn-outline-secondary" type="button" style={{ background: tema }}>Cadastrar</button>
-                    </div>
-                </form>
+export default class FormularioCadastroCliente extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      telefones: [],
+      rg: [],
+    };
+  }
+
+  adicionarTelefone = () => {
+    this.setState((prevState) => ({
+      telefones: [...prevState.telefones, ""],
+    }));
+  };
+
+  adicionarRG = () => {
+    this.setState((prevState) => ({
+      rg: [...prevState.rg, ""],
+    }));
+  };
+
+  handleChangeTelefone = (index: number, value: string) => {
+    this.setState((prevState) => {
+      const novosTelefones = [...prevState.telefones];
+      novosTelefones[index] = value;
+      return { telefones: novosTelefones };
+    });
+  };
+
+  handleChangeRg = (index: number, value: string) => {
+    this.setState((prevState) => {
+      const novoRg = [...prevState.rg];
+      novoRg[index] = value;
+      return { rg: novoRg };
+    });
+  };
+
+  render() {
+    const { tema } = this.props;
+    const { telefones, rg } = this.state;
+
+    return (
+      <div className="container-fluid">
+        <form>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Nome"
+              aria-label="Nome"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Nome social"
+              aria-label="Nome social"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+          <div className="input-group mb-3">
+            <span
+              className="input-group-text"
+              id="basic-addon1"
+              style={{ background: tema }}
+            >
+              @
+            </span>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="E-mail"
+              aria-label="E-mail"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+          <div className="input-group mb-3">
+            <input
+              type="number"
+              className="form-control"
+              placeholder="CPF"
+              aria-label="CPF"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+          {rg.map((rg, index) => (
+            <div className="input-group mb-3" key={index}>
+              <input
+                type="number"
+                className="form-control"
+                placeholder="RG"
+                aria-label="RG"
+                aria-describedby="basic-addon1"
+                value={rg}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  this.handleChangeRg(index, e.target.value)
+                }
+              />
             </div>
-        )
-    }
+          ))}
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            style={{ background: tema }}
+            onClick={this.adicionarRG}
+          >
+            Adicionar RG
+          </button>
+          {telefones.map((telefone, index) => (
+            <div className="input-group mb-3" key={index}>
+              <input
+                type="number"
+                className="form-control"
+                placeholder="Telefone"
+                aria-label="Telefone"
+                aria-describedby="basic-addon1"
+                value={telefone}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  this.handleChangeTelefone(index, e.target.value)
+                }
+              />
+            </div>
+          ))}
+          <div className="input-group mb-3">
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              style={{ background: tema }}
+              onClick={this.adicionarTelefone}
+            >
+              Adicionar Telefone
+            </button>
+          </div>
+          <div className="input-group mb-3">
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              style={{ background: tema }}
+            >
+              Cadastrar
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
--- ATVII-PL/src/componentes/formularioCadastroPet.tsx
+++ ATVII-PL/src/componentes/formularioCadastroPet.tsx
@@ -1,36 +1,144 @@
-import { Component } from "react";
+import React, { Component, ChangeEvent } from "react";
 
-type props = {
-    tema: string
-}
+type Props = {
+  tema: string;
+};
 
-export default class FormularioCadastroCliente extends Component<props> {
+type State = {
+  pets: {
+    nome: string;
+    tipo: string;
+    raca: string;
+    genero: string;
+  }[];
+};
 
-    render() {
-        let tema = this.props.tema
-        return (
-            <div className="container-fluid">
-                <form>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Digite o CPF do responsável" aria-label="CPF" aria-describedby="basic-addon1" />
-                    </div>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Nome do Pet" aria-label="Nome do Pet" aria-describedby="basic-addon1" />
-                    </div>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Raça" aria-label="Raça" aria-describedby="basic-addon1" />
-                    </div>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Gênero" aria-label="Gênero" aria-describedby="basic-addon1" />
-                    </div>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Tipo" aria-label="Tipo" aria-describedby="basic-addon1" />
-                    </div>
-                    <div className="input-group mb-3">
-                        <button className="btn btn-outline-secondary" type="button" style={{ background: tema }}>Cadastrar</button>
-                    </div>
-                </form>
+type PetField = "nome" | "tipo" | "raca" | "genero";
+
+export default class FormularioCadastroPet extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      pets: [{ nome: "", tipo: "", raca: "", genero: "" }],
+    };
+  }
+
+  adicionarPet = () => {
+    this.setState((prevState) => ({
+      pets: [...prevState.pets, { nome: "", tipo: "", raca: "", genero: "" }],
+    }));
+  };
+
+  handleChangePet = (
+    index: number,
+    campo: PetField,
+    value: string
+  ) => {
+    this.setState((prevState) => {
+      const novosPets = [...prevState.pets];
+      novosPets[index][campo] = value;
+      return { pets: novosPets };
+    });
+  };
+
+  render() {
+    const { tema } = this.props;
+    const { pets } = this.state;
+
+    return (
+      <div className="container-fluid">
+        <form>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Digite o CPF do responsável"
+              aria-label="CPF"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+          <div className="input-group mb-3">
+            <button className="btn btn-outline-secondary" type="button">
+              Buscar
+            </button>
+          </div>
+          {pets.map((pet, index) => (
+            <div key={index}>
+              <div className="input-group mb-3" key={index}>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nome do Pet"
+                  aria-label="Nome do Pet"
+                  aria-describedby="basic-addon1"
+                  value={pet.nome}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    this.handleChangePet(index, "nome", e.target.value)
+                  }
+                />
+              </div>
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Raça"
+                  aria-label="Raça"
+                  aria-describedby="basic-addon1"
+                  value={pet.raca}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    this.handleChangePet(index, "raca", e.target.value)
+                  }
+                />
+              </div>
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Gênero"
+                  aria-label="Gênero"
+                  aria-describedby="basic-addon1"
+                  value={pet.genero}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    this.handleChangePet(index, "genero", e.target.value)
+                  }
+                />
+              </div>
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Tipo"
+                  aria-label="Tipo"
+                  aria-describedby="basic-addon1"
+                  value={pet.tipo}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    this.handleChangePet(index, "tipo", e.target.value)
+                  }
+                />
+              </div>
             </div>
-        )
-    }
+          ))}
+          <div className="input-group mb-3">
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              style={{ background: tema }}
+              onClick={this.adicionarPet}
+            >
+              Adicionar Pet
+            </button>
+          </div>
+          <div className="input-group mb-3">
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              style={{ background: tema }}
+            >
+              Cadastrar
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
